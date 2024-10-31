7d1bdf4722803727a4000d7fbb6d26528f2aa74a
sakshirawat977 <rawatsakshi977@gmail.com>
2024-10-31 13:08:30 +0530

    <Link> added.

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/icons/googlelogo.svg';
 import Email from '../common/Email'; 
 import Password from '../common/Password';
@@ -38,7 +39,8 @@ export default function SignIn() {
                         <label className={`${styles.rememberMe } ${styles.bold}  custom-control-label`} htmlFor="check" style={{fontWeight:'500'}}>Remember me</label>
                     </div>
 
-                    <a href="/ForgetPassword" className={`${styles.rememberMe}`} style={{color: 'gray', fontStyle :'italic'}}>Forgot Password?</a>
+                    <Link to="/ForgetPassword" className={`${styles.rememberMe}`} style={{ color: 'gray', fontStyle: 'italic' }}>Forgot Password?</Link>
+                    
                 </div>
 
                 <div className="mb-4 d-grid gap-2 col-12 mx-auto">
@@ -64,7 +66,7 @@ export default function SignIn() {
                     <div>
                         <label className="custom-control-label" htmlFor="check" style={{color: 'gray', fontStyle :'italic', fontSize:'12px'}}>Don't have an account?</label>
                     </div>
-                    <a href="/SignUp" className="text-decoration-none" style={{color: 'orangered' , fontWeight:'700'}} >Sign up now</a>
+                    <Link to="/SignUp" className="text-decoration-none" style={{ color: 'orangered', fontWeight: '700' }}>Sign up now</Link>
                 </div>
             </form>
         </div>
--- src/component/SignUP.js
+++ src/component/SignUP.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Email from '../common/Email'; 
 import Password from '../common/Password';
 import ConfirmPW from '../common/ConfirmPW';
@@ -22,37 +23,34 @@ export default function SignUPForm() {
 
     return (
         <div className='login d-flex align-items-center justify-content-center' style={{ height: '100vh'}}>
-            <form onSubmit={handleSubmit} style = {{width:'50%',maxWidth: '500px'}}>
-                <h1 className="Heading fs-2 mb-4 fw-bold" style={{ fontFamily: "'Inknut Antiqua',serif" }}>Create Account</h1>
+            <form onSubmit={handleSubmit} style={{ width: '50%', maxWidth: '500px' }}>
+                <h1 className="Heading fs-2 mb-4 fw-bold" style={{ fontFamily: "'Inknut Antiqua', serif" }}>Create Account</h1>
 
                 <Email email={email} setEmail={setEmail} />
 
                 <Password password={password} setPassword={setPassword} />
 
-                <ConfirmPW confirmPW = {confirmPW} setConfirmPW={setConfirmPW}/>
+                <ConfirmPW confirmPW={confirmPW} setConfirmPW={setConfirmPW} />
 
                 <div className="d-flex align-items-center justify-content-around my-4 col-12">
-                    <span style={{color: 'gray', fontStyle :'italic', fontSize:'13px'}}>Enter OTP, we have sent to your email</span>
+                    <span style={{ color: 'gray', fontStyle: 'italic', fontSize: '13px' }}>Enter OTP, we have sent to your email</span>
                 </div>
 
-                <OTP length={6} onChange={(value) => setOTP(value)}/>
+                <OTP length={6} onChange={(value) => setOTP(value)} />
 
                 <div className="mb-3">
-                    <a href="/SignUp" style={{color: 'Gray'}}>RESEND OTP !</a>
+                    <Link to="/SignUp" style={{ color: 'gray' }}>RESEND OTP!</Link>
                 </div>
 
                 <div className="mb-2 d-grid gap-2 col-12 mx-auto">
-                    <button className={`${styles.ovalInput} ${styles.signIn} btn btn-light`} type="button" style={{color:'white'}}> Continue
-                </button>
+                    <button className={`${styles.ovalInput} ${styles.signIn} btn btn-light`} type="button" style={{ color: 'white' }}>Continue</button>
                 </div>
-
 
                 <div className="mb-3 d-flex justify-content-center align-items-center col-12">
                     <div>
-                        <label className="custom-control-label" htmlFor="check" style={{color: 'gray', fontStyle :'italic', fontSize:'12px'}}>I am a consistent user, </label>
-                        <a href="/SignIn" className="text-decoration-none" style={{color: 'orangered' , fontWeight:'700'}} > Sign in</a>
+                        <label className="custom-control-label" htmlFor="check" style={{ color: 'gray', fontStyle: 'italic', fontSize: '12px' }}>I am a consistent user, </label>
+                        <Link to="/SignIn" className="text-decoration-none" style={{ color: 'orangered', fontWeight: '700' }}>Sign in</Link>
                     </div>
-                    
                 </div>
             </form>
         </div>
